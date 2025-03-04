02203adb82c2fd6fc048d7603e21f5c7b3a8debe
m2tkl <kenchalros@gmail.com>
2025-03-04 21:33:50 +0900

    fix the issue where button events are not being passed

--- src/components/IconButton.vue
+++ src/components/IconButton.vue
@@ -5,16 +5,12 @@
     color="gray"
     square
     size="2xs"
-    @click="$emit('click')"
+    v-bind="$attrs"
   />
 </template>
 
 <script setup lang="ts">
 defineProps<{
   icon: string;
-}>();
-
-defineEmits<{
-  click: () => void;
 }>();
 </script>
