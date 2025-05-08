3f8c0e84d200e36b99eda6e230ac0e2f8c456a05
m2tkl <kenchalros@gmail.com>
2025-05-08 20:38:26 +0900

    lib: Add typed event bus system with helpers and type definitions

---
+++ src/types/event-types.ts
@@ -0,0 +1,10 @@
+export type EventPayloads = {
+  'app/init': undefined;
+  'workspace/switched': { workspaceSlug: string };
+  'workspace/created': undefined;
+  'workspace/deleted': undefined;
+  'memo/switched': { workspaceSlug: string; memoSlug: string };
+  'memo/created': { workspaceSlug: string };
+  'memo/updated': { workspaceSlug: string; memoSlug: string };
+  'memo/deleted': { workspaceSlug: string };
+};
