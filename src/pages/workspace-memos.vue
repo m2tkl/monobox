27d396ad4bc41309a4e8107f32a49dba906c6c58
m2tkl <kenchalros@gmail.com>
2025-06-07 21:59:48 +0900

    feat: Add section into memo list page

--- src/pages/workspace-memos.vue
+++ src/pages/workspace-memos.vue
@@ -2,15 +2,79 @@
   <NuxtLayout name="default">
     <template #main>
       <div class="size-full">
-        <div class="size-full px-4 pb-8 pt-4">
+        <div class="size-full px-4 pb-8 overflow-y-auto">
           <!-- Memo List -->
           <LoadingSpinner v-if="workspaceMemosLoading" />
           <p v-else-if="memos.length === 0">
             No memos
           </p>
-          <ul class="grid grid-cols-[repeat(auto-fill,minmax(150px,1fr))] gap-3 pb-4">
+
+          <section v-if="bookmarks.length > 0">
+            <div class="sticky top-0 z-10 bg-surface">
+              <div class="flex h-12 items-center">
+                <UIcon
+                  :name="iconKey.bookmark"
+                  class="mr-2"
+                />
+                <h2 class="font-bold text-gray-600">
+                  Bookmarks
+                </h2>
+              </div>
+            </div>
+
+            <ul class="grid grid-cols-[repeat(auto-fill,minmax(150px,1fr))] gap-3">
+              <li
+                v-for="memo in bookmarks"
+                :key="memo.id"
+                class="aspect-[1/1] overflow-hidden rounded-lg"
+              >
+                <NuxtLink :to="`/${route.params.workspace}/${memo.slug_title}`">
+                  <UCard
+                    class="aspect-[1/1] hover:bg-slate-100"
+                    :ui="{
+                      header: 'px-3 pt-3 pb-0 sm:px-3 divide-white',
+                      body: 'px-3 pb-4 pt-1 sm:p-3 divide-white',
+                      root: 'divide-white hover:divide-slate-100',
+                    }"
+                  >
+                    <template #header>
+                      <h3 class="truncate-multiline text-sm font-semibold text-gray-700">
+                        {{ truncateString(memo.title, 32) }}
+                      </h3>
+                    </template>
+                    <img
+                      v-if="memo.thumbnail_image"
+                      :src="transformImageSrc(memo.thumbnail_image)"
+                    >
+                    <p
+                      v-for="p in truncateString(memo.description ? memo.description : '', 128)?.split('\n')"
+                      v-else
+                      :key="p"
+                      class="truncate-multiline text-sm text-gray-500"
+                    >
+                      {{ p }}
+                    </p>
+                  </UCard>
+                </NuxtLink>
+              </li>
+            </ul>
+          </section>
+
+          <div class="sticky top-0 z-10 bg-surface">
+            <div class="flex h-12 items-center">
+              <UIcon
+                :name="iconKey.recent"
+                class="mr-2"
+              />
+              <h2 class="font-bold text-gray-600">
+                Recent
+              </h2>
+            </div>
+          </div>
+
+          <ul class="grid grid-cols-[repeat(auto-fill,minmax(150px,1fr))] gap-3">
             <li
-              v-for="memo in memos"
+              v-for="memo in recentMemos"
               :key="memo.id"
               class="aspect-[1/1] overflow-hidden rounded-lg"
             >
@@ -90,6 +154,11 @@ const store = useWorkspaceStore();
 const workspace = computed(() => store.workspace);
 const memos = computed(() => store.workspaceMemos);
 const workspaceMemosLoading = computed(() => store.workspaceMemosLoading);
+
+const recentMemos = computed(() => {
+  return store.workspaceMemos.filter(memo => !store.bookmarkedMemos.map(item => item.title).includes(memo.title));
+});
+const bookmarks = computed(() => store.bookmarkedMemos);
 </script>
 
 <style scoped>
